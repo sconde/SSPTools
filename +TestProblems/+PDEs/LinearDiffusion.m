030165911bf2693cdf065c9d498d8e7b1157093a
Sidafa Conde <sconde@umassd.edu>
2016-04-28 19:06:59 -0600

    add LinearDiffusion (this closes #16)
    this allows the user to define the advection-diffusion linear problem

---
+++ +TestProblems/+PDEs/LinearDiffusion.m
@@ -0,0 +1,25 @@
+classdef LinearDiffusion < handle
+    
+    properties
+       name = 'Linear Diffusion';
+       f;
+       haveExact = false;
+       eqn;
+       isLinear = true;
+       nu;
+    end
+    
+    methods
+        function obj = LinearDiffusion(varargin)
+            p = inputParser;
+            p.KeepUnmatched = true;
+            
+            addParameter(p, 'nu', 0.01);
+            p.parse(varargin{:});
+
+            
+            obj.nu = p.Results.nu;
+            obj.f = @(t, u) -obj.nu *u;
+        end
+    end
+end
