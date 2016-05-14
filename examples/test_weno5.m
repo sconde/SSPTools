1aaf3df091eea373e46001ae166ea2ec367aefe2
Sidafa Conde <sconde@umassd.edu>
2016-05-13 19:39:01 -0600

    test_weno5 example

--- examples/test_weno5.m
+++ examples/test_weno5.m
@@ -3,7 +3,7 @@ clear all; close all; clc
 addpath('../')
 A = [0 0;1 0]; b = [1 0]; s = 2; %TODO: infert s from size(A,1)
 At = [0 0;0 1]; bt = [0 1];
-N = 10;
+N = 50;
 
 dt = 0.01;
 Tfinal = 2;
@@ -21,10 +21,12 @@ dfdx = WenoCoreDan.Weno5('N', N, 'domain', [-1, 1],...
 dudt = SSPTools.Steppers.ERK('A', A, 'b',b, 's', s,...
     'dfdx', dfdx, 'ExplicitProblem', imp_pro, 'y0', y0);
 
-% line1 = plot(dfdx.x, dudt.y0(dfdx.x),'-r','linewidth',2);
-% axis([-1 1 0 1]);
+line1 = plot(dfdx.x, dudt.y0(dfdx.x),'-r','linewidth',2);
+axis([-1 1 0 1]);
 
-%dudt.takeStep(0.001)
+% dudt.takeStep(0.001)
+
+
 
 
 while t < Tfinal
