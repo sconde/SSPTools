65207ca5b81754aad27631eba51689b23f3e202d
Sidafa Conde <sconde@umassd.edu>
2016-05-29 20:19:59 -0600

    test problems from kupka's imex paper

---
+++ +TestProblems/+ODEs/KupkaExplicit.m
@@ -0,0 +1,36 @@
+classdef KupkaExplicit < TestProblems.ODEs.ODE
+    
+    properties
+        a;
+        isLinear;
+        exact;
+    end
+    
+    methods
+        
+        function obj = KupkaExplicit(varargin)
+            
+            obj = obj@TestProblems.ODEs.ODE(varargin{:});
+            p = inputParser;
+            p.KeepUnmatched = true;
+            
+            
+            addParameter(p, 'a', 2);
+            p.parse(varargin{:});
+            
+            obj.name = 'Kupka-Explicit';
+            obj.isLinear = false;
+            
+            obj.a = p.Results.a;
+            obj.f = @(t, u) (1 + sin(u));
+            obj.exact = @(t,u) zero(size(u));
+        end
+        
+    end
+    
+    methods (Access = protected)
+        function dudt = rhs(ep, u)
+            dudt = [u(2);(1/ep)*(-u(1) + (1 - u(1)^2)*u(2))];
+        end
+    end
+end
