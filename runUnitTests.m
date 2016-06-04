01f036ee1eb71ad639c6b402420c8e918fa9df62
Sidafa Conde <sconde@umassd.edu>
2016-06-03 18:13:52 -0600

    making a cell of tests to run
    this will clean up things a bit

--- runUnitTests.m
+++ runUnitTests.m
@@ -1,25 +1,38 @@
 clear all; close all; clc
 
-% run all the convergence tests
-test1 = UnitTests.TestOrder;
-res = run(test1); res.table
+cellOfTest = {UnitTests.TestOrder; ...  % convergence tests
+    UnitTests.TestTVD; ...              % SSP Tests
+    UnitTests.TestODEs; ...             % ODE Tets
+    UnitTests.TestSSPTools; ...         % run-time Tests
+    UnitTests.TestMesh; ...             % Mesh1D Tests
+    UnitTests.TestDG;                   % DG Runtime
+    };
 
-% run all the ssp tests
-test2 = UnitTests.TestTVD;
-res2 = run(test2); res2.table
-
-% run all the ODE tests
-test3 = UnitTests.TestODEs;
-res3 = run(test3); res3.table
-
-% run all run-time test 
-test4 = UnitTests.TestSSPTools;
-res4 = run(test4); res4.table
-
-% run Mesh1D tests
-test5 = UnitTests.TestMesh;
-res5 = run(test5); res5.table
-
-% run TestDG with FE
-test6 = UnitTests.TestDG;
-res6 = run(test6); res6.table
+for testInd = 1:numel(cellOfTest)
+   test = cellOfTest{testInd};
+   res = run(test); res.table
+end
+    
+% % run all the convergence tests
+% test1 = UnitTests.TestOrder;
+% res = run(test1); res.table
+% 
+% % run all the ssp tests
+% test2 = UnitTests.TestTVD;
+% res2 = run(test2); res2.table
+% 
+% % run all the ODE tests
+% test3 = UnitTests.TestODEs;
+% res3 = run(test3); res3.table
+% 
+% % run all run-time test 
+% test4 = UnitTests.TestSSPTools;
+% res4 = run(test4); res4.table
+% 
+% % run Mesh1D tests
+% test5 = UnitTests.TestMesh;
+% res5 = run(test5); res5.table
+% 
+% % run TestDG with FE
+% test6 = UnitTests.TestDG;
+% res6 = run(test6); res6.table
