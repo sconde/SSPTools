d3a4a748d51ac893a8926294b0fc2459a4ab3b67
Sidafa Conde <sconde@umassd.edu>
2016-06-03 18:14:54 -0600

    cleaning up

--- examples/test_ssp.m
+++ examples/test_ssp.m
@@ -1,38 +1,27 @@
 clear all; close all; clc
 
 addpath('../');
-N = 300;
-
-%testing = 'ERK';
-testing = 'DIRK';
+N = 1000;
 
 y0 = @(x) heaviside(x - (ceil((x+1)/2) -1)*2);
 
-imp_pro = TestProblems.PDEs.LinearAdvection('a', 1);
+exp_pro = TestProblems.PDEs.LinearAdvection('a', 1);
+
+imp_pro = TestProblems.PDEs.LinearAdvection('a', 10);
 
 dfdx = SSPTools.Discretizers.FiniteDifference('N', N, 'domain', [-1, 1],...
+    'bc','periodic','Problem', exp_pro);
+
+dgdx = SSPTools.Discretizers.FiniteDifference('N', N, 'domain', [-1, 1],...
     'bc','periodic','Problem', imp_pro);
 
-if strcmpi(testing, 'erk')
-    
-    
-    dudt = SSPTools.Steppers.LoadERK('MethodName', 'FE',...
-        'dfdx', dfdx, 'y0', y0);
-    
-    tvdPDE = Tests.SSP('integrator', dudt,'TVD',true,'CFLRefinement',0.01,...
-        'CFLMAX',1.1,'CFL',0.85);
-    
-elseif strcmpi(testing,'dirk')
-    
-    dudt = SSPTools.Steppers.LoadDIRK('MethodName', 'MidPoint',...
-        'dfdx', dfdx, 'y0', y0);
-    
-    tvdPDE = Tests.SSP('integrator', dudt,'TVD',true,'CFLRefinement',0.1,...
-        'CFLMAX',2,'CFL',0.85);
-end
 
-tvbPDE = Tests.SSP('integrator', dudt,'TVB',true,'CFLRefinement',0.1,...
-    'CFLMAX',2,'CFL',0.8);
+dudt = SSPTools.Steppers.LoadIMEX('MethodName', 'arslpum',...
+    'dfdx', dfdx, 'dgdx', dgdx, 'y0', y0);
+
+tvdPDE = Tests.SSP('integrator', dudt,'TVD',true,'CFLRefinement',0.01,...
+    'CFLMAX',1.1,'CFL',0.01);
 
 tvdPDE.run();
 tvdPDE.plotSolution();
+tvdPDE.ssp
