12eb1c9a7c871473706685b2698091ec338d5c54
Sidafa Conde <sconde@umassd.edu>
2016-06-05 13:34:14 -0600

    add ODE convergence test in the unittest

--- +UnitTests/TestODEs.m
+++ +UnitTests/TestODEs.m
@@ -26,7 +26,7 @@ classdef TestODEs < matlab.unittest.TestCase
             
             while t < Tfinal
                 
-                y0 = dudt.takeStep(dt);
+                dudt.takeStep(dt);
                 t = t+ dt;
             end
             
@@ -53,13 +53,61 @@ classdef TestODEs < matlab.unittest.TestCase
             
             while t < Tfinal
                 
-                y0 = dudt.takeStep(dt);
+                dudt.takeStep(dt);
                 t = t+ dt;
             end
             
-            testCase.assertTrue(true)
-            assertTrue(testCase, true)
+            testCase.assertTrue(true);
+            assertTrue(testCase, true);
             
+        end
+        
+        function testVanderpolSDIRK23(testCase)
+            import matlab.unittest.TestCase
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            
+            Tfinal = 2;
+
+            y0 = [2; -0.6654321];
+            
+            vdp = TestProblems.ODEs.Vanderpol();
+            
+            dudt = SSPTools.Steppers.LoadDIRK('MethodName','SDIRK23',...
+                'ODE', vdp, 'y0', y0);
+            
+            convergence_pro = Tests.Convergence('integrator', dudt,'Tfinal', Tfinal,...
+                'DT', Tfinal./(200:20:300));
+            
+            convergence_pro.run();
+            obsOrder = convergence_pro.getOrder('l2');
+            expectedOrder = 3;
+            testCase.assertThat(obsOrder, IsEqualTo(expectedOrder, ...
+                'Within', AbsoluteTolerance(0.2)))
+        end
+        
+        function testVanderpolSDIRK22(testCase)
+            import matlab.unittest.TestCase
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            
+            Tfinal = 2;
+            
+            y0 = [2; -0.6654321];
+            
+            vdp = TestProblems.ODEs.Vanderpol();
+            
+            dudt = SSPTools.Steppers.LoadDIRK('MethodName','SDIRK22',...
+                'ODE', vdp, 'y0', y0);
+            
+            convergence_pro = Tests.Convergence('integrator', dudt,'Tfinal', Tfinal,...
+                'DT', Tfinal./(160:20:220));
+            
+            convergence_pro.run();
+            obsOrder = convergence_pro.getOrder('l2');
+            expectedOrder = 2;
+            testCase.assertThat(obsOrder, IsEqualTo(expectedOrder, ...
+                'Within', AbsoluteTolerance(0.2)))
         end
         
         function testKupkaOrderSSP22(testCase)
@@ -78,7 +126,7 @@ classdef TestODEs < matlab.unittest.TestCase
                 'ODE', exp_pro, 'ImplicitODE', imp_pro, 'y0', y0);
             
             convergencePDE = Tests.Convergence('integrator', dudt,'Tfinal', Tfinal,...
-                'DT', Tfinal./[40:10:100],'ExactSolution',@(t) tan(t));
+                'DT', Tfinal./(40:10:100),'ExactSolution',@(t) tan(t));
             convergencePDE.run();
             obsOrder = convergencePDE.getOrder('l2');
             testCase.assertThat(obsOrder, IsEqualTo(2, ...
