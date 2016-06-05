5bb7391645a3ea03751bd169b6856813b5562465
Sidafa Conde <sconde@umassd.edu>
2016-06-05 14:20:30 -0600

    some ode convergence test
    :xa

--- examples/test_vanderpol.m
+++ examples/test_vanderpol.m
@@ -5,6 +5,7 @@ clear all; close all; clc
 % TODO: fix ODEs
 
 addpath('../');
+addpath('~/Dropbox/SSPTools/');
 method = '~/Dropbox/imex-linear/src/butcher-optimization/Method/DIRK/G/Pex2/Pim2/Plin4/S5/K1/method_r1_0.5451888988915_acc_-15.mat';
 rk = load(method);
 A = rk.A; b = rk.b; s = rk.s;
@@ -15,22 +16,14 @@ N = 100;
 dt = 0.01;
 Tfinal = 2;
 t = 0;
-f = @(u) u;
-
-testing = 'ERK';
-%testing = 'DIRK';
 
 y0 = [2; -0.6654321];
 
 vdp = TestProblems.ODEs.Vanderpol();
 
-if strcmpi(testing, 'erk')
-    dudt = SSPTools.Steppers.ERK('A', A, 'b',b, 's', s,...
-        'ODE', vdp, 'y0', y0);
-else
-    dudt = SSPTools.Steppers.DIRK('A', At, 'b',bt, 's', s,...
-        'ODE', vdp, 'y0', y0);
-end
+dudt = SSPTools.Steppers.DIRK('A', At, 'b',bt, 's', s,...
+    'ODE', vdp, 'y0', y0);
+
 
 plot(t, y0(1),'sr');
 hold on
