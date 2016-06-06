19f5257e4ccc7b8573ec90c69a30dfe6affb70d6
Sidafa Conde <sconde@umassd.edu>
2016-06-06 11:44:14 -0600

    add am_radius method to calculate SSP coeff of a method
    this is D. Ketcheson's code

--- +SSPTools/+Steppers/RK.m
+++ +SSPTools/+Steppers/RK.m
@@ -5,17 +5,18 @@ classdef RK < handle
         dfdt;
         dfdx;
         ExplicitProblem;
-        A = []; b = []; c = []; alpha = []; s = []; r = [];
+        A = []; b = []; c = []; alpha = []; s = []; 
+        r;
         p; % Nonlinear-Order
         plin; % Linear-Order
         t0;
         y0;
         t;
+        isSSP;
     end
     
     properties (Access = protected)
         steps = 1;
-        isSSP;
         L;
         CFL;
         dx;
@@ -33,8 +34,6 @@ classdef RK < handle
             addParameter(p,'name','MSRK');
             addParameter(p, 'dfdx', []);
             addParameter(p, 'dfdt', []);
-%             addParameter(p, 'ExplicitProblem', []);
-%             addParameter(p, 'ImplicitProblem', []);
             addParameter(p, 'A', []);
             addParameter(p, 'b', []);
             addParameter(p, 'r', -Inf);
@@ -46,7 +45,7 @@ classdef RK < handle
             addParameter(p, 'y0', []);
             addParameter(p, 'ODE', []);
             p.parse(varargin{:});
-            
+                       
             if isa(p.Results.dfdt, 'function_handle')
                 obj.dydt = p.Results.dfdt;
             end
@@ -57,47 +56,52 @@ classdef RK < handle
             else
                 obj.y0 = p.Results.y0(:);
             end
-                        
+            
             obj.t = p.Results.t;
             obj.A = p.Results.A;
             obj.b = p.Results.b;
             obj.c = sum(obj.A,2);
             obj.s = numel(obj.b); %infer the number of stages from the length of b
             obj.p = p.Results.p;
-            obj.r = p.Results.r;
+                        
+            % get the SSP coefficient of the method
+            if ~isempty(p.Results.r) && ~isinf(p.Results.r)
+                obj.r = p.Results.r;
+                obj.isSSP = true;
+            else
+                obj.r = obj.am_radius(obj.A, obj.b(:));
+                if obj.r > 0
+                    obj.isSSP = true;
+                end
+            end
             
             if isempty(p.Results.plin)
                 obj.plin = obj.p;
             else
                 obj.plin = p.Results.plin;
             end
-
+            
             obj.alpha = p.Results.alpha;
             obj.name = p.Results.name;
             obj.dfdx = p.Results.dfdx;
             obj.t0 = p.Results.t0;
-                                    
+            
             assert(isequal(obj.s, size(p.Results.A,1)),...
                 sprintf('RK A:Stage-count -- Num-Rows(A) != %d',obj.s));
             
-            if ~isempty(p.Results.r)
-                obj.r = p.Results.r;
-                obj.isSSP = true;
-            end
-                        
-            if ~isempty(obj.dfdx)%.problem)
+            if ~isempty(obj.dfdx)
                 obj.ExplicitProblem = obj.dfdx.problem;
             elseif ~isempty(p.Results.ODE)
                 obj.ExplicitProblem = p.Results.ODE;
             end
-                           
+            
             if isa(obj.ExplicitProblem, 'TestProblems.ODEs.ODE')
                 obj.L = @(t,y) obj.ExplicitProblem.f(t,y);
                 obj.CFL = [];
                 obj.dx = [];
                 obj.x = [];
             else
-                obj.L = @(t,y) obj.dfdx.L(t, y); 
+                obj.L = @(t,y) obj.dfdx.L(t, y);
                 obj.dx = obj.dfdx.dx;
                 obj.x = p.Results.dfdx.x(:); % make it a vector
             end
@@ -132,14 +136,53 @@ classdef RK < handle
             if ~isempty(obj.dfdx) && (obj.dfdx.systemSize > 1)
                 y = reshape(y, obj.dfdx.nx, obj.dfdx.systemSize);
             end
-%             %Q = [r ru E];
-%             density   = u(1:obj.N);
-%             momentum  = u(obj.N+1:2*(obj.N));
-%             energy    = u(2*(obj.N)+1:3*(obj.N));
-%             keyboard
+            %             %Q = [r ru E];
+            %             density   = u(1:obj.N);
+            %             momentum  = u(obj.N+1:2*(obj.N));
+            %             energy    = u(2*(obj.N)+1:3*(obj.N));
+            %             keyboard
         end
         
+        function r = am_radius(obj, A,b)
+            %function r = am_radius(A,b)
+            %
+            %By David Ketcheson
+            %
+            %Evaluates the Radius of absolute monotonicity
+            %of a Runge-Kutta method, given the Butcher array.
+            %
+            %For an m-stage method, A should be an m x m matrix
+            %and b should be a column vector of length m.
+            %
+            %Accuracy can be changed by modifying the value of eps.
+            %Methods with very large radii of a.m. (>50) will require
+            %rmax to be increased.
+            
+            rmax=50; eps=1.e-12;
+            
+            m=length(b); e=ones(m,1);
+            K=[A;b'];
+            rlo=-50; rhi=rmax;
+            
+            while rhi-rlo>eps  %use bisection
+                r=0.5*(rhi+rlo);
+                X=eye(m)+r*A; beta=K/X; ech=r*K*(X\e);
+                if (min(beta(:))<-3.e-16 || max(ech(:))>1.+3.e-16)
+                    rhi=r;
+                else
+                    rlo=r;
+                end
+            end
+                        
+            if rhi==rmax % r>=rmax
+                error('Error: increase value of rmax in am_radius.m');
+            else
+                r=rlo;
+            end
+            
+        end
     end
+    
     
     methods ( Access = private )
         
@@ -148,5 +191,4 @@ classdef RK < handle
         end
         
     end
-    
 end
