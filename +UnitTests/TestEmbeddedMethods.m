54f46b5301a018bc3ab245e9603ceeae26c43e4e
Sidafa Conde <sconde@umassd.edu>
2016-06-07 09:36:49 -0600

    adding TestEmbeddedMethods to unittests
    class of tests for embedded rk methods. right now, its is only explicit
    methods

---
+++ +UnitTests/TestEmbeddedMethods.m
@@ -0,0 +1,76 @@
+classdef TestEmbeddedMethods < matlab.unittest.TestCase
+    
+    properties
+        
+    end
+    
+    % Test Method Block
+    methods (Test)
+        % includes unit test functions
+        
+        function testMerson45(testCase)
+            import matlab.unittest.TestCase
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            
+            N = 16;
+            Tfinal = 0.4;
+            
+            
+            y0 = @(x) sin(x);
+            
+            
+            exp_pro = TestProblems.PDEs.LinearAdvection('a', 1);
+            dfdx = SSPTools.Discretizers.Spectral('derivativeOrder',1, 'N', N,...
+                'Problem', exp_pro);
+            
+            dudt = SSPTools.Steppers.LoadERK('MethodName', 'Merson45',...
+                'dfdx', dfdx, 'y0', y0);
+            
+            convergencePDE = Tests.Convergence('integrator', dudt,'Tfinal', Tfinal,...
+                'CFL', (1/2).^(1:5));
+            
+            espectedOrder = 4;
+            convergencePDE.run();
+            obsOrder = convergencePDE.getOrder('L2');
+            
+            testCase.verifyTrue(dudt.isEmbedded, true); % should be an embedded-RK
+            
+            testCase.assertThat(obsOrder, IsEqualTo(espectedOrder, ...
+                'Within', AbsoluteTolerance(0.2)))
+        end
+        
+        function testZonneveld43(testCase)
+            import matlab.unittest.TestCase
+            import matlab.unittest.constraints.IsEqualTo
+            import matlab.unittest.constraints.AbsoluteTolerance
+            
+            N = 16;
+            Tfinal = 0.4;
+            
+            
+            y0 = @(x) sin(x);
+            
+            
+            exp_pro = TestProblems.PDEs.LinearAdvection('a', 1);
+            dfdx = SSPTools.Discretizers.Spectral('derivativeOrder',1, 'N', N,...
+                'Problem', exp_pro);
+            
+            dudt = SSPTools.Steppers.LoadERK('MethodName', 'Zonneveld43',...
+                'dfdx', dfdx, 'y0', y0);
+            
+            convergencePDE = Tests.Convergence('integrator', dudt,'Tfinal', Tfinal,...
+                'CFL', (1/2).^(1:5));
+            
+            espectedOrder = 4;
+            convergencePDE.run();
+            obsOrder = convergencePDE.getOrder('L2');
+            
+            testCase.verifyTrue(dudt.isEmbedded, true); % should be an embedded-RK
+            
+            testCase.assertThat(obsOrder, IsEqualTo(espectedOrder, ...
+                'Within', AbsoluteTolerance(0.2)))
+        end
+        
+    end
+end
