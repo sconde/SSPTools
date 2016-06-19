fbbe469e99042bee6dae8aba149f1e8d74ec445a
Sidafa Conde <sconde@umassd.edu>
2016-06-18 23:58:51 -0600

    fixing the aren problem definition

--- +TestProblems/+ODEs/Aren.m
+++ +TestProblems/+ODEs/Aren.m
@@ -2,6 +2,7 @@ classdef Aren < TestProblems.ODEs.ODE
     
     properties
         isLinear;
+        mu;
     end
     
     methods
@@ -9,14 +10,17 @@ classdef Aren < TestProblems.ODEs.ODE
         function obj = Aren(varargin)
             
             obj = obj@TestProblems.ODEs.ODE(varargin{:});
+            
             p = inputParser;
             p.KeepUnmatched = true;
             
+            addParameter(p, 'mu', 0.012277471);
             
             p.parse(varargin{:});
             
             obj.name = 'Aren';
             obj.isLinear = false;
+            obj.mu = p.Results.mu;
             
 %             %f = zeros(size(y));
 %             amu = 0.012277471;
@@ -37,9 +41,8 @@ classdef Aren < TestProblems.ODEs.ODE
     methods %(Access = protected)
         
         function f = flux(obj, t, y)
-            amu = 0.012277471;
-            amup = 1.0 - amu;
-            sqr = y(1) + amu;
+            amup = 1.0 - obj.mu;
+            sqr = y(1) + obj.mu;
             r1 = sqr*sqr + y(2)*y(2);
             r1 = r1 * sqrt(r1);
             sqr = y(1) - amup;
@@ -48,8 +51,8 @@ classdef Aren < TestProblems.ODEs.ODE
             
             f = [y(3);
                 y(4);
-                y(1) + 2.0 * y(4) - amup * (y(1)+amu) / r1 - amu * (y(1)-amup) / r2;
-                y(2) - 2.0 * y(3) - amup * y(2) / r1 - amu * y(2) / r2];
+                y(1) + 2.0 * y(4) - amup * (y(1)+obj.mu) / r1 - obj.mu * (y(1)-amup) / r2;
+                y(2) - 2.0 * y(3) - amup * y(2) / r1 - obj.mu * y(2) / r2];
         end
         
     end
