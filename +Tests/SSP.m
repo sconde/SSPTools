6fc5494c78004a3eba3a4780143e3bbe4b739142
Sidafa Conde <sconde@umassd.edu>
2016-06-25 19:28:37 -0600

    changed the conditions to eliminate unnessecary computations

--- +Tests/SSP.m
+++ +Tests/SSP.m
@@ -32,7 +32,8 @@ classdef SSP < Tests.Test
         startingr;
         lambda;
         cfl_refinement;
-        %TV;
+        acc;
+        tol;
     end
     
     
@@ -53,6 +54,7 @@ classdef SSP < Tests.Test
             p.addParameter('TVD', false);
             p.addParameter('CFLMAX', 1.5);
             p.addParameter('CFLRefinement',0.1);
+            p.addParameter('Tolerance', 1e-5);
             p.addParameter('r', 1);
             p.parse(varargin{:});
             
@@ -69,6 +71,7 @@ classdef SSP < Tests.Test
             obj.CFLMAX = p.Results.CFLMAX;
             obj.cflRefine = p.Results.CFLRefinement;
             obj.theortical_r = p.Results.r;
+            obj.tol = p.Results.Tolerance;
             
             %TODO: is this correct?
             %assert(any([obj.testTVD obj.testTVB]),'Must choice TVD or TVB');
@@ -89,7 +92,7 @@ classdef SSP < Tests.Test
         end
         
         function initialize(obj)
-            obj.startingr = min(obj.theortical_r,.005);
+            obj.startingr = max(obj.theortical_r/2,.005);
             %obj.sp = min(obj.theortical_r/10,.005);
             obj.lambda = linspace(obj.startingr, obj.theortical_r + 0.5, 10);
             obj.cfl_refinement = max(diff(obj.lambda));
@@ -104,27 +107,41 @@ classdef SSP < Tests.Test
             coarse = 1;
             VV_ = [];
             lambda_ = obj.lambda;
-                        
-            while obj.cfl_refinement > 1e-10
+                     
+            obj.acc = -5;
+            numRefinement = 0;
+            while obj.cfl_refinement > obj.tol
                 Violation_ = obj.runRange(lambda_);
                 
                 L = [L,lambda_];
                 VV_ = [VV_, Violation_];
                 
+                
                 % find the first violation
-                ind = find(log10(Violation_) > -12, 1, 'first');
+                ind = find(log10(Violation_) > obj.acc, 1, 'first');
                 
                 if isempty(ind);  %If the observed CFL is outside original range
                     maxL = max(lambda_);
                     ind = find(lambda_ == maxL, 1, 'first');
                     lambda_ = linspace(maxL - obj.cfl_refinement, maxL + 2*obj.cfl_refinement,10);
                 else
+
                     Ltemp = sort(L);
                     ind_ = find(Ltemp == lambda_(ind),1,'first');
-                    newL = Ltemp(ind_);
-                    lambda_ = linspace(newL-2*obj.cfl_refinement,newL + 2*obj.cfl_refinement,10); % need to make sure this is all positive
+                    
+                    if ind_ > 1
+                     lambda_ = sort(linspace(Ltemp(ind_ - 1), Ltemp(ind_), 5))
+                    else
+                        keyboard
+                        newL = Ltemp(ind_);
+                        lambda_ = linspace(newL-2*obj.cfl_refinement,newL,10); % need to make sure this is all positive
+                    end
                 end
-                obj.cfl_refinement = max(diff(lambda_));                
+                obj.cfl_refinement = max(abs(diff(lambda_)));
+                obj.acc = min(obj.acc, floor(log10(obj.cfl_refinement)) - 1) ;
+                acct = obj.acc;
+                numRefinement = numRefinement + 2;
+                ref = obj.cfl_refinement;
             end
             
             obj.CFL = L;
@@ -137,22 +154,34 @@ classdef SSP < Tests.Test
         function Violation_ = runRange(obj, lambda)
             tvdFun = @(u) sum([abs(diff(u)); abs((u(1)-u(end)))]);
             
-            Violation_ = nan(1, length(lambda));
+            Violation_ = inf(1, length(lambda));
             nSteps = obj.Steps;
             for i = 1:numel(lambda);
                 dt = lambda(i)*obj.dudt.dfdx.dx;
                 
-                obj.dudt.resetInitCondition();
-                [~, y_] = obj.dudt.getState();
-                TV_ = nan(1, nSteps);
-                TV_(1) = tvdFun(y_);
-                
-                for tt = 2:nSteps
-                    obj.dudt.takeStep(dt);
+                try
+                    obj.dudt.resetInitCondition();
                     [~, y_] = obj.dudt.getState();
-                    TV_(tt) = tvdFun(y_);
+                    TV_ = nan(1, nSteps);
+                    TV_(1) = tvdFun(y_);
+                    
+                    for tt = 2:nSteps
+                        obj.dudt.takeStep(dt);
+                        [~, y_] = obj.dudt.getState();
+                        TV_(tt) = tvdFun(y_);
+                    end
+                    maxDiff = max([diff(TV_),1e-15]);
+                    Violation_(i) = maxDiff;
+                    log10(maxDiff); %TODO: delete this line
+
+                    if log10(maxDiff) > obj.acc
+                        break
+                    end
+                    
+                catch err
+                    keyboard
+                    break
                 end
-                Violation_(i) = max([diff(TV_),1e-15]);
             end
         end
                     
